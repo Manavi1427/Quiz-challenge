82310e0b7b5022cc3373cc691c641c3b1721adce
Manavi1427 <manavisarkar136@gmail.com>
2024-08-26 11:51:03 +0530

    questions 1

--- quiz.js
+++ quiz.js
@@ -4,3 +4,48 @@ function signup(){
     sp.style.display="block"
     rp.style.display="none"
 }
+let questions=[
+    {
+        prompt:"What is a data structure?",
+        options:["A programming language", 
+            "collection of algorithms",
+            "A way to store and organize data",
+            "A type of computer hardware"],
+        answer:"A way to store and organize data"
+    },
+    {
+        prompt: `How do you call a function named myFunction?`,
+        options: [
+            "call myFunction()",
+            "myFunction()",
+            "call function myFunction",
+            "Call.myFunction",
+        ],
+        answer: "myFunction()",
+    },
+    {
+        prompt: `Inside which HTML element do we put the JavaScript?`,
+        options: [
+            "<javascript>",
+            "<js>",
+            "<script>",
+            "<scripting>",
+        ],
+        answer: "<script>",
+    },
+    {
+        prompt: `In JavaScript, which of the following is a logical operator?`,
+        options: ["|", "&&", "%", "/"],
+        answer: "&&",
+    },
+    {
+        prompt: `A named element in a JavaScript program that is used to store and retrieve data is a _____.`,
+        options: [
+            "method",
+            "assignment operator",
+            "letiable",
+            "string",
+        ],
+        answer: "letiable",
+    },
+];
